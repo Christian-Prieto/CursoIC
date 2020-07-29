a79d506eb5fe8acfd0963eacc326723be6feb90e
Christian-Prieto <christian-amado.prieto@es.gfi.world>
2020-07-29 11:57:56 +0000

    asdasd

---
+++ webapp/target/classes/Calculadora.java
@@ -0,0 +1,13 @@
+package com.gfi;
+
+public class Calculadora
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public static int (int num1, int num2){
+        return num1+num2;
+    }
+}
